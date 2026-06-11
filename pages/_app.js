20874029fc755a7e79567837f677baec3c77ee15
Abdalaye SOUMARE <abdalaye.soumare.ext@ademe.fr>
2026-06-11 11:44:53 +0200

    fix(font) integration Arial flash loaded by using next/font/local

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,36 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NextAdapter } from 'next-query-params'
 import { QueryParamProvider } from 'use-query-params'
 import { init } from '@socialgouv/matomo-next'
+import localFont from 'next/font/local'
 
 import { GlobalStyle } from 'utils/styles'
 
 import { StyleProvider } from 'components/providers/StyleProvider'
 import { ModalProvider } from 'components/providers/ModalProvider'
 import { DataProvider } from 'components/providers/DataProvider'
+
+const marianne = localFont({
+  src: [
+    {
+      path: '../public/fonts/Marianne-Light.woff2',
+      weight: '300',
+      style: 'normal',
+      display: 'swap',
+    },
+    {
+      path: '../public/fonts/Marianne-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+      display: 'swap',
+    },
+    {
+      path: '../public/fonts/Marianne-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+      display: 'swap',
+    }
+  ],
+})
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient())
@@ -20,7 +44,7 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return (
-    <QueryParamProvider adapter={NextAdapter}>
+    <QueryParamProvider className={marianne.className} adapter={NextAdapter}>
       <QueryClientProvider client={queryClient}>
         <StyleProvider>
           <DataProvider>
