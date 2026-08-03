9f24b2cd8921485f1f953af270a41758ee14aeab
Abdalaye SOUMARE <abdalaye.soumare.ext@ademe.fr>
2026-08-03 14:47:29 +0200

    Fix(WAT-100) replace "le" by "mon"

--- src/components/views/home/OpenSetupProfileButton.js
+++ src/components/views/home/OpenSetupProfileButton.js
@@ -20,7 +20,7 @@ export default function OpenSetupProfileButton() {
           setProfileOccurrences()
         }}
       >
-        Modifier le profil
+        Modifier mon profil
       </Button>
     </Button.Wrapper>
   )
